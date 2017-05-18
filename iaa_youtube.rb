fac2dbb6fe3149af140a25b7cb6288f7c7f65846
dariasteam <dariasteam94@gmail.com>
2017-05-18 19:49:03 +0100

    Más cosas

--- iaa_youtube.rb
+++ iaa_youtube.rb
@@ -1,8 +1,7 @@
 require 'csv'
 
-module FileTid
-  content = ""
-  filenames = [
+module IAA
+  youtube = [
     "cine.csv",
     "deportes.csv",
     "musica.csv",
@@ -10,23 +9,46 @@ module FileTid
     "review.csv",
     "tutorial.csv",
     "videojuegos.csv",
-    "vlog.csv"
+    "vlog.csv",
+    "animales.csv",
+    "humor.csv"
+  ]
+  listas = [
+    "listas/lista_animales.csv",
+    "listas/lista_consolas.csv",
+    "listas/lista_deportes.csv",
+    "listas/lista_musica.csv",
+    "listas/lista_peliculas.csv"
   ]
 
-  filenames.map { |name|
+  file_o = File.open("output.csv", 'w')
+
+  youtube.map { |name|
     if (File.file?(name))
-      file_1 = File.open(name, 'r')
+      file = File.open(name, 'r')
       puts "Extrayendo información de #{name}"
       file_1.readline
       while !file_1.eof?
         line = CSV.parse(file_1.readline)
-        content += "\"#{line[0][1]}\",\"#{line[0][3]}\"\n"
+        file_o.puts "\"#{line[0][1]}\",\"#{line[0][3]}\"\n"
       end
     else
-      puts "Error abriendo los archivos"
+      puts "Error abriendo el archivo #{name}"
     end
   }
-  file_2 = File.open("output.csv", 'w')
-  file_2.puts content
+
+  listas.map { |name|
+    if (File.file?(name))
+      file = File.open(name, 'r')
+      puts "Extrayendo información de #{name}"
+      file_1.readline
+      while !file_1.eof?
+        line = CSV.parse(file_1.readline)
+        file_o.puts "\"#{line[0][0]}\",\"#{line[0][1]}\"\n"
+      end
+    else
+      puts "Error abriendo el archivo #{name}"
+    end
+  }
   puts "Fichero generado con éxito"
 end
